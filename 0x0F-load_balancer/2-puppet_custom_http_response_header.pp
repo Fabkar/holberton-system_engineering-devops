7c81551bee1988b6d3b464caa9a1bc34f57e4437
Fabkar <2003@holbertonschool.com>
2020-12-21 23:12:14 -0500

    update

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,9 +1,9 @@
 # Configure trought Puppet a header in Nginx server
 
-exec { 'Header_add':
-  command => 'sudo apt-get update';
-  sudo apt-get -y install nginx;
-  sudo sed -i "/server_name _/a add_header X-Served-By $hostname;" etc/nginx/sites-available/default;
+exec { 'Header adition':
+  command => 'sudo apt-get update -y;
+  sudo apt-get install nginx -y;
+  sudo sed -i "/server_name _/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
   sudo service nginx restart',
   provider => 'shell',
 }
