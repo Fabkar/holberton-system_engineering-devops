39b468b3e4818547500ea77a7595fba51af9c274
Fabkar <fab_862@hotmail.com>
2021-02-01 16:04:55 -0500

    update with optimize

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -2,7 +2,6 @@
 """  using this REST API, for a given employee ID,
 returns information about his/her TODO list progress"""
 import requests
-import json
 from sys import argv
 
 
@@ -11,10 +10,8 @@ if __name__ == '__main__':
     user = (requests.get(url + 'users',
             params={"id": argv[1]}).json())[0].get("name")
     todos = requests.get(url + 'todos', params={'userId': argv[1]}).json()
-    list_obj = []
-    for i, object in enumerate(todos):
-        if todos[i].get("completed") is True:
-            list_obj.append("\t "+object.get("title"))
+    list_obj = [("\t "+object.get("title")) for i, object in enumerate(todos)
+                if todos[i].get("completed") is True]
     print('Employee {} is done with tasks({}/{}):'.format(
         user, len(list_obj), len(todos)))
     print("\n".join(list_obj))
