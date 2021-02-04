ec0c34ecbd148738c88d3e77c6315b43844c31c6
Fabkar <fab_862@hotmail.com>
2021-02-04 13:14:57 -0500

    return the numbers of subscribers from a reddit api

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,14 @@
+#!/usr/bin/pyhton3
+""" function that queries the Reddit API and returns
+the number of subscribers"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Method that return the number of subscribers from an Reddit API"""
+    header = {"User-agent": "darth"}
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    response = (requests.get(url, headers=header))
+    if response.status_code != 200:
+        return 0
+    return response.json().get('data').get('subscribers')
