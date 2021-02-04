fddefc2c17f7b4739616d1f02977d102506db3e4
Fabkar <fab_862@hotmail.com>
2021-02-04 13:17:14 -0500

    shows the titles of the firts 10 hot posts

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Reddit Api
+"""
+import requests
+
+
+def top_ten(subreddit):
+    """ prints the titles of the first 10 hot posts
+    listed for a given subreddit."""
+    header = {"User-agent": "darth"}
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    response = requests.get(url, headers=header)
+    if response.status_code != 200:
+        print("None")
+    data = response.json().get('data').get('children')
+    for i in data[:10]:
+        print(i.get('data').get('title'))
