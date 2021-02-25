8e148ce093d905da692a8311470a33578ea29816
Fabkar <fab_862@hotmail.com>
2021-02-25 17:45:34 -0500

    changes

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,6 @@
-#This puppet modify limit of request
-exec { 'aynoolimit':
-    command => 'sudo sed -i "s/15/15000/" /etc/default/ngnix;
+# set up limits to request
+exec { 'increasing_limits':
+    command => 'sudo sed -i "s/15/15000/" /etc/default/nginx;
                 sudo service nginx restart',
     path    => ['/usr/bin', '/usr/sbin', '/usr/local/bin', '/usr/local/sbin'],
 }
